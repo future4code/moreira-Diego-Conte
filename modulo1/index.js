75d5c0d60e13a68e05dc26ea46546b1730cbb0c5
Diego Conte <diegocomte@gmail.com>
2021-11-29 14:38:09 -0300

    Branch Variaveis e exercício 29nov21 JS

---
+++ modulo1/index.js
@@ -0,0 +1,68 @@
+//EXERCÍCIOS DE INTEPRETAÇÃO DE CÓDIGO//
+
+/*Exercício1
+O console imprime inicialmente 10 relativo à variável 'b'.
+Depois, imprime os valores 10, 5 relativo à 'a' e 'b'.
+
+Exercício 2
+O console imprime 10, 10, 10.
+
+Exercício 3
+O programa solicita a carga horária trabalhada e a remuneração diára.
+Com isso, calcula o quanto o funcionário recebe por hora.
+
+let cargaHorariaDiaria = prompt("Quantas horas você trabalha por dia?")
+let remuneraDiaria = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${remuneraDiaria/cargaHorariaDiaria} por hora`) */
+
+//EXERCÍCIOS DE ESCRITA DE CÓDIGO//
+
+//Exercício 1//
+let nome1
+let idade1
+
+console.log(typeof nome1, typeof idade1)
+
+/*O console imprimirá 'undefined undefined' porque não houve nenhuma atribuição
+ de valor. Destaca-se que 'undefined' não é o mesmo de 'null'. */
+
+ nome1 = prompt("Seja bem-vindo. Qual é o seu nome?")
+ idade1 = prompt("E qual é a sua idade?")
+
+ console.log(typeof nome1, typeof idade1)
+
+ /* Agora, o console imprimirá 'string string' porque fez-se uso do prompt,
+ o que sempre retorna valores 'string'. */
+
+ console.log("Olá,", nome1,", você tem", idade1, "anos.")
+
+ //Exercício 2//
+
+ let almocouHoje = prompt("Você já almoçou hoje?")
+ let codouHoje = prompt("Você já programou em JavaScript hoje?")
+ let aulaFer = prompt("Você gostou da aula da Fer hoje?")
+
+ console.log("Você já almoçou hoje?", almocouHoje)
+ console.log("Você já programou em JavaScript hoje?", codouHoje)
+ console.log("Você gostou da aula da Fer hoje?", aulaFer)
+
+ //Exercício 3//
+ 
+let a = 10
+let b = 25
+
+let c = a
+a = b
+b = c
+
+console.log("O novo valor de a é", a) 
+console.log("O novo valor de b é", b)
+
+
+//Desafio//
+
+let valorUser1 = prompt("Olá. Informe o primeiro valor:")
+let valorUser2 = prompt("Agora, informe o segundo valor:")
+
+console.log("O primeiro número somado ao segundo resulta em", valorUser1+valorUser2)
+console.log("O primeiro número multiplicado pelo segundo resulta em", valorUser1*valorUser2)
