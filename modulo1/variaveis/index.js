d0fa6d486443b377091c079579df5946be60a3d9
Diego Conte <diegocomte@gmail.com>
2021-11-30 14:25:08 -0300

    Operadores_30-11-21

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*EXERCÍCIOS DE INTERPRETAÇÃO
 
 Exercício 1
@@ -20,11 +21,34 @@ alert('Voce recebe ${horasDiaTrabalhadas/remuneraDiaria} por hora') */
 
 /*EXRCÍCIOS DE ESCRITA DE CÓDIGO
 Exercício 1*/
+=======
+//EXERCÍCIOS DE INTEPRETAÇÃO DE CÓDIGO//
+
+/*Exercício1
+O console imprime inicialmente 10 relativo à variável 'b'.
+Depois, imprime os valores 10, 5 relativo à 'a' e 'b'.
+
+Exercício 2
+O console imprime 10, 10, 10.
+
+Exercício 3
+O programa solicita a carga horária trabalhada e a remuneração diára.
+Com isso, calcula o quanto o funcionário recebe por hora.
+
+let cargaHorariaDiaria = prompt("Quantas horas você trabalha por dia?")
+let remuneraDiaria = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${remuneraDiaria/cargaHorariaDiaria} por hora`) */
+
+//EXERCÍCIOS DE ESCRITA DE CÓDIGO//
+
+//Exercício 1//
+>>>>>>> 6b3350a9b0c44fcabf6a742dc571876e99c69603
 let nome1
 let idade1
 
 console.log(typeof nome1, typeof idade1)
 
+<<<<<<< HEAD
 /* O console apresentará undefined undefined, isso porque as variáveis estão vazias,
 sem valores atribuídos. */
 
@@ -58,12 +82,60 @@ a = b
 b = exchanger
 
 console.log("O novo valor de a é", a)
+=======
+/*O console imprimirá 'undefined undefined' porque não houve nenhuma atribuição
+ de valor. Destaca-se que 'undefined' não é o mesmo de 'null'. */
+
+ nome1 = prompt("Seja bem-vindo. Qual é o seu nome?")
+ idade1 = prompt("E qual é a sua idade?")
+
+ console.log(typeof nome1, typeof idade1)
+
+ /* Agora, o console imprimirá 'string string' porque fez-se uso do prompt,
+ o que sempre retorna valores 'string'. */
+
+ console.log("Olá,", nome1,", você tem", idade1, "anos.")
+
+ //Exercício 2//
+
+ let almocouHoje = "Você já almoçou hoje?"
+ let codouHoje = "Você já programou em JavaScript hoje?"
+ let aulaFer = "Você gostou da aula da Fer hoje?"
+
+ let respostaAlmoco = prompt(almocouHoje)
+ let respostaCodou = prompt(codouHoje)
+ let respostaAula = prompt(aulaFer)
+
+ console.log(almocouHoje, respostaAlmoco)
+ console.log(codouHoje, respostaCodou)
+ console.log(aulaFer, respostaAula)
+
+ //Exercício 3//
+ 
+let a = 10
+let b = 25
+
+let c = a
+a = b
+b = c
+
+console.log("O novo valor de a é", a) 
+>>>>>>> 6b3350a9b0c44fcabf6a742dc571876e99c69603
 console.log("O novo valor de b é", b)
 
 
 //Desafio//
+<<<<<<< HEAD
 let valorUser1 = Number(prompt("Olá. Informe o primeiro valor:"))
 let valorUser2 = Number(prompt("Agora, informe o segundo valor:"))
 
 console.log("O primeiro número somado ao segundo resulta em:", valorUser1+valorUser2)
 console.log("O primeiro número multiplicado pelo segundo resulta em:", valorUser1*valorUser2)
+=======
+
+let valorUser1 = Number(prompt("Olá. Informe o primeiro valor:"))
+let valorUser2 = Number(prompt("Agora, informe o segundo valor:"))
+
+console.log("O primeiro número somado ao segundo resulta em", valorUser1+valorUser2)
+console.log("O primeiro número multiplicado pelo segundo resulta em", valorUser1*valorUser2)
+>>>>>>> 6b3350a9b0c44fcabf6a742dc571876e99c69603
