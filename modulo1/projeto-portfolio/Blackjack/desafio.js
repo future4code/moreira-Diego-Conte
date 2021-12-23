951b2223d78dc88a57373412fad296f313cdde14
Diego Conte <diegocomte@gmail.com>
2021-12-23 11:16:55 -0300

    Bugs fixed

--- modulo1/projeto-portfolio/Blackjack/desafio.js
+++ modulo1/projeto-portfolio/Blackjack/desafio.js
@@ -1,45 +1,6 @@
-//-------------------------------------------------------------- VARs
-let cardUser = [];
-let cardPc = [];
-let scoreUser = [];
-let scorePc = [];
-let allUserSuits = " ";
-let allPcSuits = " ";
-
-//-------------------------------------------------------------- MINOR FUNCTIONS
-const arrayOfPoints = (item) => { // Return all points in array
-   return item.valor
-}
-
-const arrayOfSuits = (item) => { // Return all suits in array
-   return item.texto
-}
-
-function totalScore(array) { // Return total points
-   let totalPoints = 0
-   for (let i = 0; i < array.length; i++) {
-      totalPoints = totalPoints + array[i]
-   } return totalPoints
-}
-
-function suits(array) { // Return string of suits
-   let suits = ""
-   for (let i = 0; i < array.length; i++) {
-      suits = suits + array[i] + "  "
-   } return suits
-}
-
-function returnAllUserSuits() { // Calls functions 'arrayOfSuits' and 'suits' to return user's suits
-   return ((allUserSuits = cardUser.map(arrayOfSuits)), allUserSuits = suits(allUserSuits))
-}
-
-function returnAllPCSuits() { // Calls functions 'arrayOfSuits' and 'suits' to return Pc's suits
-   return ((allPcSuits = cardPc.map(arrayOfSuits)), allPcSuits = suits(allPcSuits))
-}
-
 //-------------------------------------------------------------- START
 let welcomeQuestion = confirm(`Boas-vindas ao jogo de Blackjack! 
-Quer iniciar uma nova rodada?`);
+Deseja iniciar a rodada?`);
 
 if (welcomeQuestion === true) {
    myBlackjack()
@@ -49,13 +10,55 @@ if (welcomeQuestion === true) {
 
 //-------------------------------------------------------------- MAIN FUNCTION
 function myBlackjack() {
+
+
+   //-------------------------------------------------------------- VARs
+   let cardUser = [];
+   let cardPc = [];
+   let scoreUser = [];
+   let scorePc = [];
+   let allUserSuits = " ";
+   let allPcSuits = " ";
+
+   //-------------------------------------------------------------- MINOR FUNCTIONS
+   const arrayOfPoints = (item) => { // Return all points in array
+      return item.valor
+   }
+
+   const arrayOfSuits = (item) => { // Return all suits in array
+      return item.texto
+   }
+
+   function totalScore(array) { // Return total points
+      let totalPoints = 0
+      for (let i = 0; i < array.length; i++) {
+         totalPoints = totalPoints + array[i]
+      } return totalPoints
+   }
+
+   function suits(array) { // Return string of suits
+      let suits = ""
+      for (let i = 0; i < array.length; i++) {
+         suits = suits + array[i] + "  "
+      } return suits
+   }
+
+   function returnAllUserSuits() { // Calls functions 'arrayOfSuits' and 'suits' to return user's suits
+      return ((allUserSuits = cardUser.map(arrayOfSuits)), allUserSuits = suits(allUserSuits))
+   }
+
+   function returnAllPCSuits() { // Calls functions 'arrayOfSuits' and 'suits' to return Pc's suits
+      return ((allPcSuits = cardPc.map(arrayOfSuits)), allPcSuits = suits(allPcSuits))
+   }
+
+
    let i = 0
    while (i < 2) {
       cardUser.push(comprarCarta())
       cardPc.push(comprarCarta())
       i++
    }
-  
+
    if ((cardUser[0].texto === "A" && cardUser[1].texto === "A") || (cardPc[0].texto === "A" && cardPc[1].texto === "A")) {
       alert(`A combinação atual de cartas não é permitida porque impossibilita a primeira rodada. Reinicie o jogo.`)
       throw new Error("Needs to restart due to combination of cards");
@@ -74,7 +77,7 @@ function myBlackjack() {
       "O computador revelou a carta " + cardPc[0].texto + "\n" +
       "\n" +
       "Deseja tirar outra carta?")
-  
+
 
    while (anotherCard === true && scoreUser <= 21) {
       cardUser.push(comprarCarta())
@@ -118,4 +121,12 @@ function myBlackjack() {
             "O computador ganhou!")
       }
    }
+
+   let playAgain = confirm(`Deseja iniciar uma nova rodada?`);
+   if (playAgain === true) {
+      myBlackjack()
+   } else {
+      alert("O jogo encerrou. Você pode fechar esta página.")
+   }
 }
+
