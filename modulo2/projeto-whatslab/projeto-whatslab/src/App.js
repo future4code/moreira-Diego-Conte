5332a926f11f2c43af236921099d89cef0d66714
Diego Conte <diegocomte@gmail.com>
2022-01-15 19:59:35 -0300

    Finalização

--- modulo2/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import React from 'react';
-import ContainerMessages from './components/css'
+import ContainerMessages from './components/css';
+import iconButton from './img/sendIcon.png';
+
+//Compreendo que o ideal seria separar cada componente e estilização em arquivos diversos; desta vez, porém, não o pude 
+//fazer por conta das dificuldades da semana que me exauriram. Posto isso, separei somente a estilização.
 
 class App extends React.Component {
 
@@ -10,7 +14,6 @@ class App extends React.Component {
 
     message: [
       {
-
       }
     ]
   }
@@ -24,52 +27,62 @@ class App extends React.Component {
   })
 
   addMessage = () => {
-    const newMessage = [...this.state.message,
-    { nameUser: this.state.inputName, userMessage: this.state.inputMessage }];
-
+    let newMessage = [...this.state.message,
+    {nameUser: this.state.inputName + ":", 
+    userMessage: this.state.inputMessage }];
+    
+    if(this.state.inputName !== '' && this.state.inputMessage !== ''){ //Sets the state and prevents empty submissions
     this.setState({
       message: newMessage,
       inputMessage: ""
-    });
+    })};
+  }
+
+  enterSend = (event) => { //Submissions by enter
+    if (event.key === "Enter") {
+      this.addMessage()
+    }
   }
 
   render() {
-    const allMessages = this.state.message.map((message) => {
-      return (
-        <div key={Date.now}>
-          <p className='nameUser'>{message.nameUser}</p>
-          <p>{message.userMessage}</p>
-        </div>
-      )
-    })
 
-    document.addEventListener("keypress", function (e) {
-      if (e.key === "Enter") {
-        const submit = document.querySelector("#send")
-        submit.click()
+    const allMessages = this.state.message.map((msg) => {
+      if(msg.nameUser === "eu:" || msg.nameUser === "Eu:") { //Adds ':'. Does not accept the method .toLowerCase(). Do not know why :(
+        return (
+          <container><p>{msg.userMessage}</p></container>
+        )
+      } else {
+        return (
+          <div key={Date.now}>
+            <p className='nameUser'>{msg.nameUser}</p>
+            <p>{msg.userMessage}</p>
+          </div>
+        )
       }
-    });
+    })
 
     return (
       <ContainerMessages>
         {allMessages}
         <section>
           <input className='inputName'
-          type="text"
+            type="text"
             placeholder={"Nome"}
             value={this.state.inputName}
             onChange={this.getName}
+            onKeyDown={this.enterSend}
           />
 
           <input required className='inputMessage'
             placeholder={"Sua mensagem"}
             value={this.state.inputMessage}
             onChange={this.getMessage}
+            onKeyDown={this.enterSend}
           />
 
-          <button type="submit" onClick={this.addMessage}></button>
+          <button type="submit" onClick={this.addMessage}> <img src={iconButton} alt='Icone enviar' /> </button>
         </section>
-      </ContainerMessages>
+      </ContainerMessages >
     )
   }
 }
--- modulo2/projeto-whatslab/projeto-whatslab/src/components/css.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/components/css.js
@@ -1,84 +1,107 @@
 import styled from 'styled-components';
-import imgBack from '../img/background.jpg'
-import IconeSend from '../img/sendIcon.png'
 
-const ContainerMessages = styled.div`
-display: flex;
-flex-direction: column;
-min-height: 95vh;
-width: 500px;
-margin: 10px auto;
-justify-content: end;
-align-items: flex-start;
-border-radius: 3px;
-border: 1px solid gray;
-background-image: url(${imgBack});
-background-size: 100%; 
-
-div{
+const ContainerMessages = styled.div` //Main screen
   display: flex;
   flex-direction: column;
-  margin: 15px 5px;
+  min-height: 95vh;
+  width: 515px;
+  margin: 10px auto;
+  justify-content: end;
+  align-items: flex-start;
+  border-radius: 10px;
+  border: 2px solid gray;
+  word-wrap: break-word;
+  background-color: lightgrey;
+
+  container{ //Display box-messages to the right
+    display: flex;
+    width: 60%;
+    flex-direction: column;
+    margin: 15px 5px;
+    margin-left: 200px;
+    text-align: end;
+    word-wrap: break-word; 
     
-  .nameUser{
-    margin: 2px 10px;
-    font-size: 17px;
-    font-weight: bold;
+    p{
+      margin: 2px 10px;
+      font-size: 17px;
+    }
   }
 
-  p{
-    margin: 2px 10px;
-    font-size: 17px;
+
+  div{ //Layout box-messages to the left
+    display: flex;
+    width: 60%;
+    flex-direction: column;
+    margin: 15px 5px;
+    margin-right: 200px;
+    word-wrap: break-word;
+      
+    .nameUser{
+      margin: 2px 10px;
+      font-size: 17px;
+      font-weight: bold;
+    }
+
+    p{
+      margin: 2px 10px;
+      font-size: 17px;
+      font-weight: lighter ;
+    }
   }
-}
 
-section{
-  display: flex;
-  height: 95%px;
-  width: 500px;
-  justify-content: center;
-  align-items: flex-end;
-}
-  
-.inputName{
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  font-size: 15px;
-  width: 6vw;
-   height: 4.5vh;
-  border-radius: 4px;
-  border: 1px solid gray;
-  margin: 10px 5px;
-}
+  section{ //Display box-fields: name, message and button
+    border-top: 1px solid gray;
+    display: flex;
+    height: 95%px;
+    width: 500px;
+    justify-content: center;
+    align-items: flex-end;
+  }
+    
+  .inputName{
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 15px;
+    width: 6vw;
+    height: 4.5vh;
+    border-radius: 4px;
+    border: 1px solid gray;
+    margin: 10px 5px;
+  }
 
-.inputMessage{
-  justify-content: center;
-  align-items: center;
-  text-align: left;
-  font-size: 15px;
-  width: 100vw;
-  height: 4.5vh;
-  border-radius: 4px;
-  border: 1px solid gray;
-  margin: 10px 5px;
-}
+  .inputMessage{
+    justify-content: center;
+    align-items: center;
+    text-align: left;
+    font-size: 15px;
+    width: 100vw;
+    height: 4.5vh;
+    border-radius: 4px;
+    border: 1px solid gray;
+    margin: 10px 5px;
+  }
 
-button{
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  font-size: 15px;
-  background-image: url(${IconeSend});
-  background-size: 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  width: 80px;
-  height: 5vh;
-  border-radius: 4px;
-  border: 1px solid gray;
-  margin: 10px 5px;
-}
+  img{
+    display: flex;
+    border-radius: 4px;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 30px;
+  }
+
+  button{
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 40px;
+    height: 32px;
+    margin: 10px 5px;
+    border: none;
+    background-color: inherit;
+} 
 `
 
 export default ContainerMessages
