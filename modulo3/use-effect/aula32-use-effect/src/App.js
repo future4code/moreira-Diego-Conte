9c213793b13138325a622495bebd656906d76429
Diego Conte <diegocomte@gmail.com>
2022-02-08 14:30:03 -0300

    Aula Hooks e userEffect()

---
+++ modulo3/use-effect/aula32-use-effect/src/App.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import PokeCard from "./Components/PokeCard/PokeCard";
+import Styled from './styles'
+
+function App() {
+  const [pokeList, setPokeList] = useState([]);
+  const [pokeName, setPokeName] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((response) => {
+        // função que está setando no estado os 151 pokemons
+        setPokeList(response.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const changePokeName = (event) => {
+    setPokeName(event.target.value);
+  };
+
+  return (
+    <Styled>
+      <select onChange={changePokeName}>
+        <option value={""}>Nenhum</option>
+        {/* renderizando a lista de pokemons como opções do select */}
+        {pokeList.map((pokemon) => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          );
+        })}
+      </select>
+
+      {pokeName && <PokeCard pokemon={pokeName} />}
+    </Styled>
+  );
+}
+
+export default App;
---
+++ modulo3/use-effect/aula32-use-effect/src/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+const Styled = styled.div` 
+font-family: sans-serif;
+text-align: center;
+`
+
+export default Styled
